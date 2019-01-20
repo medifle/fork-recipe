1cb57944aa6b6122bdbdefcbb9ddeb5c56d76d52
medifle <13296643+medifle@users.noreply.github.com>
2019-01-19 19:00:11 -0500

    fix($client): fix 'recipeName' variable scope
    and reformat using prettier

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-(function() {
+;(function() {
   const form = document.getElementById('form')
   const recipe = document.getElementById('recipe')
   const h1 = document.querySelector('h1')
@@ -30,19 +30,19 @@
   }
 
   function getRecipe(e) {
-    recipeName = recipe.value.trim()
-    recipe.value = ''
+    let recipeName = recipe.value.trim()
+    recipe.value = '' // reset input
 
     h1.textContent = 'Loading...'
-    container.innerHTML = ''
+    container.innerHTML = '' // reset container content
 
-    // ajax call
+    // AJAX call
     let xhr = new XMLHttpRequest()
     xhr.onreadystatechange = () => {
       if (xhr.readyState == 4 && xhr.status == 200) {
         let response = JSON.parse(xhr.responseText)
         // console.log(response)
-        if (response.error === "limit") {
+        if (response.error === 'limit') {
           h1.textContent = 'Max query hit, please come tomorrow :)'
           return
         }
@@ -52,16 +52,14 @@
     }
     xhr.open('POST', `/ingredients`)
     xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8')
-    xhr.send(JSON.stringify({ ingredient: recipeName }))
+    xhr.send(JSON.stringify({ingredient: recipeName}))
 
     e.preventDefault()
     e.stopPropagation()
   }
 
-
   // start after DOM is ready
   document.addEventListener('DOMContentLoaded', () => {
     form.addEventListener('submit', getRecipe)
   })
-
-}())
+})()
